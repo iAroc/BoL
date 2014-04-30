f3b1480f5f6b1530455f571f22bad10aeae27e15
Superx321 <Superx321>
2014-04-30 23:10:50 +0200

    .

--- ShadowVayneBeta.lua
+++ ShadowVayneBeta.lua
@@ -1,7 +1,7 @@
 --[[
 
 	Shadow Vayne Script by Superx321
-	Version: 2.13
+	Version: 2.14
 
 	Functions:
 	- AntiCapCloser with Settings
@@ -82,6 +82,8 @@ function GetUpdate()
 				print("<font color=\"#F0Ff8d\"><b>ShadowVayne:</b></font> <font color=\"#FF0F0F\">New Version ("..(ServerVersion)..") available, downloading...</font>")
 				DownloadFile(SHADOWVAYNE_SCRIPT_URL, SHADOWVAYNE_PATH, function () print("<font color=\"#F0Ff8d\"><b>ShadowVayne:</b></font> <font color=\"#FF0F0F\">Updated to Version "..(ServerVersion)..". Please reload with F9</font>") end)
 				AlreadyChecked = true
+			else
+				print("<font color=\"#F0Ff8d\"><b>ShadowVayne:</b></font> <font color=\"#FF0F0F\">No Updates available</font>")
 			end
 		end
 	end
