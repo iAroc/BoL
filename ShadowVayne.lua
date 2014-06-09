74f2b229eb7ac7afc3af5480dcecd9a57329a8cf
Superx321 <flo_lenggi@hotmail.com>
2014-06-09 08:56:59 +0200

    ShadowVayne 3.17

--- ShadowVayne.lua
+++ ShadowVayne.lua
@@ -1,7 +1,7 @@
 --[[
 
 	Shadow Vayne Script by Superx321
-	Version: 3.16
+	Version: 3.17
 
 	For Functions & Changelog, check the Thread on the BoL Forums:
 	http://botoflegends.com/forum/topic/18939-shadow-vayne-the-mighty-hunter/
