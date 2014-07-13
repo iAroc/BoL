d7c87a603b9ad2da98ab0c1b820533e35648280c
Superx321 <flo_lenggi@hotmail.com>
2014-07-13 22:50:06 +0200

    CustomPermaShow 1.07

--- common/CustomPermaShow.lua
+++ common/CustomPermaShow.lua
@@ -1,52 +1,107 @@
-version = 1.06
+
+version = 1.07
 
 if not _G.HidePermaShow then
 	_G.HidePermaShow = {}
 end
 
-function CustomPermaShow(TextVar, ValueVar, VisibleVar, PermaColorVar, OnColorVar, OffColorVar, IndexVar)
-	if not _G._CPS_Added then
-		if not DrawCustomText then
-			_G.DrawCustomText = _G.DrawText
-			_G.DrawText = function(Arg1, Arg2, Arg3, Arg4, Arg5) _DrawText(Arg1, Arg2, Arg3, Arg4, Arg5) end
-			_G.DrawCustomLine = _G.DrawLine
-			_G.DrawLine = function(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6) _DrawLine(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6) end
-			OldPermaShowTable, OldPermaShowCount, IsPermaShowStatusOn, PermaShowTable = {}, 0, {}, {}
-			AddDrawCallback(_DrawCustomPermaShow)
-			_G._CPS_Added = true
-		else
-			OldPermaShowTable, OldPermaShowCount, IsPermaShowStatusOn, PermaShowTable = {}, 0, {}, {}
-			AddDrawCallback(_DrawCustomPermaShow)
-			_G._CPS_Added = true
-		end
-	end
+if not _G.CPS then
+	_G.CPS = {}
+	_G.CPS.Index = {}
+	_G.CPS.NoIndex = {}
+	_G.CPS.StartY = GetSave("scriptConfig")["Master"].py
+	_G.CPS.LastCheck = 0
+	_G._DrawText = DrawText
+	DelayAction(function() 	_G.CPS.OldCountDone = true end, 3)
+	DelayAction(function() 	AddDrawCallback(_DrawCustomPermaShow) end, 0.1)
+end
 
-	if IndexVar == nil then
-		local _CPS_Updated = false
-		for i=1, #PermaShowTable do
-			if PermaShowTable[i]["TextVar"] == TextVar then
-				PermaShowTable[i]["ValueVar"], PermaShowTable[i]["VisibleVar"],_CPS_Updated = ValueVar,VisibleVar,true
-				PermaShowTable[i]["PermaColorVar"],PermaShowTable[i]["OnColorVar"],PermaShowTable[i]["OffColorVar"] = PermaColorVar, OnColorVar, OffColorVar
+function CustomPermaShow(TextVar, ValueVar, VisibleVar, PermaColorVar, OnColorVar, OffColorVar, IndexVar)
+	if IndexVar then
+		local ItsNew = true
+		for i = 1,#_G.CPS.Index do
+			if _G.CPS.Index[i].IndexVar == IndexVar then
+				ItsNew = false
+				_G.CPS.Index[i].TextVar = TextVar
+				_G.CPS.Index[i].ValueVar = ValueVar
+				_G.CPS.Index[i].VisibleVar = VisibleVar
+				_G.CPS.Index[i].PermaColorVar = PermaColorVar
+				_G.CPS.Index[i].OnColorVar = OnColorVar
+				_G.CPS.Index[i].OffColorVar = OffColorVar
+				break
 			end
 		end
 
-		if not _CPS_Updated then
-			PermaShowTable[#PermaShowTable+1] = {["TextVar"] = TextVar, ["ValueVar"] = ValueVar, ["VisibleVar"] = VisibleVar, ["PermaColorVar"] = PermaColorVar, ["OnColorVar"] = OnColorVar, ["OffColorVar"] = OffColorVar}
+		if ItsNew then
+			table.insert(_G.CPS.Index, {
+			["TextVar"] = TextVar,
+			["ValueVar"] = ValueVar,
+			["VisibleVar"] = VisibleVar,
+			["PermaColorVar"] = PermaColorVar,
+			["OnColorVar"] = OnColorVar,
+			["OffColorVar"] = OffColorVar,
+			["IndexVar"] = IndexVar,
+			})
 		end
 	else
-		local _CPS_Updated = false
-		for i=1, #PermaShowTable do
-			if PermaShowTable[i]["IndexVar"] == IndexVar then
-				PermaShowTable[i]["ValueVar"], PermaShowTable[i]["VisibleVar"],_CPS_Updated = ValueVar,VisibleVar,true
-				PermaShowTable[i]["PermaColorVar"],PermaShowTable[i]["OnColorVar"],PermaShowTable[i]["OffColorVar"] = PermaColorVar, OnColorVar, OffColorVar
-				PermaShowTable[i]["TextVar"] = TextVar
+		local ItsNew = true
+		for i = 1,#_G.CPS.NoIndex do
+			if _G.CPS.NoIndex[i].TextVar == TextVar then
+				ItsNew = false
+				_G.CPS.NoIndex[i].ValueVar = ValueVar
+				_G.CPS.NoIndex[i].VisibleVar = VisibleVar
+				_G.CPS.NoIndex[i].PermaColorVar = PermaColorVar
+				_G.CPS.NoIndex[i].OnColorVar = OnColorVar
+				_G.CPS.NoIndex[i].OffColorVar = OffColorVar
+				break
 			end
 		end
 
-		if not _CPS_Updated then
-			PermaShowTable[#PermaShowTable+1] = {["TextVar"] = TextVar, ["ValueVar"] = ValueVar, ["VisibleVar"] = VisibleVar, ["PermaColorVar"] = PermaColorVar, ["OnColorVar"] = OnColorVar, ["OffColorVar"] = OffColorVar, ["IndexVar"] = IndexVar}
+		if ItsNew then
+			table.insert(_G.CPS.NoIndex, {
+			["TextVar"] = TextVar,
+			["ValueVar"] = ValueVar,
+			["VisibleVar"] = VisibleVar,
+			["PermaColorVar"] = PermaColorVar,
+			["OnColorVar"] = OnColorVar,
+			["OffColorVar"] = OffColorVar,
+			})
 		end
 	end
+end
+
+function _GetPermaColor(PermaTable)
+	if PermaTable.ValueVar == true then
+		if PermaTable.OnColorVar == nil then
+			if PermaTable.PermaColorVar == nil then
+				ColorVar = _CPS_Master.color.green
+			else
+				ColorVar = PermaTable.PermaColorVar
+			end
+		else
+			ColorVar = PermaTable.OnColorVar
+		end
+		TextVar = "      ON"
+	elseif PermaTable.ValueVar == false then
+		if PermaTable.OffColorVar == nil then
+			if PermaTable.PermaColorVar == nil then
+				ColorVar = _CPS_Master.color.lgrey
+			else
+				ColorVar = PermaTable.PermaColorVar
+			end
+		else
+			ColorVar = PermaTable.OffColorVar
+		end
+		TextVar = "      OFF"
+	else
+		if PermaTable.PermaColorVar == nil then
+			ColorVar = _CPS_Master.color.lgrey
+		else
+			ColorVar = PermaTable.PermaColorVar
+		end
+		TextVar = PermaTable.ValueVar
+	end
+	return TextVar,ColorVar
 end
 
 function _DrawCustomPermaShow()
@@ -59,104 +114,51 @@ function _DrawCustomPermaShow()
 	_CPS_Master.cellSize = _CPS_Master.fontSize + 1
 	_CPS_Master.width = WINDOW_W and math.round(WINDOW_W / 6.4) or 160
 	_CPS_Master.row = _CPS_Master.width * 0.7
-
-	for i = 1, #PermaShowTable do
-		if PermaShowTable[i].ValueVar == true then
-			if PermaShowTable[i].OnColorVar == nil then
-				if PermaShowTable[i].PermaColorVar == nil then
-					ColorVar = _CPS_Master.color.green
-				else
-					ColorVar = PermaShowTable[i].PermaColorVar
-				end
-			else
-				ColorVar = PermaShowTable[i].OnColorVar
-			end
-			TextVar = "      ON"
-		elseif PermaShowTable[i].ValueVar == false then
-			if PermaShowTable[i].OffColorVar == nil then
-				if PermaShowTable[i].PermaColorVar == nil then
-					ColorVar = _CPS_Master.color.lgrey
-				else
-					ColorVar = PermaShowTable[i].PermaColorVar
-				end
-			else
-				ColorVar = PermaShowTable[i].OffColorVar
-			end
-			TextVar = "      OFF"
-		else
-			if PermaShowTable[i].PermaColorVar == nil then
-				ColorVar = _CPS_Master.color.lgrey
-			else
-				ColorVar = PermaShowTable[i].PermaColorVar
-			end
-			TextVar = PermaShowTable[i].ValueVar
-		end
-		if PermaShowTable[i]["VisibleVar"] then
-			if not (_G.HidePermaShow[PermaShowTable[i].TextVar] ~= nil and _G.HidePermaShow[PermaShowTable[i].TextVar] == true) and not _G.HideCustomPermaShow and string.len(PermaShowTable[i].TextVar) > 4 then
-				DrawCustomLine(_CPS_Master.px - 1, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.px + _CPS_Master.row - 1, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.cellSize, _CPS_Master.color.lgrey)
-				DrawCustomText(PermaShowTable[i].TextVar, _CPS_Master.fontSize, _CPS_Master.px, _CPS_Master.py1, _CPS_Master.color.grey)
-				DrawCustomLine(_CPS_Master.px + _CPS_Master.row, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.px + _CPS_Master.width + 1, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.cellSize, ColorVar)
-				DrawCustomText(TextVar, _CPS_Master.fontSize, _CPS_Master.px + _CPS_Master.row + 1, _CPS_Master.py1, _CPS_Master.color.grey)
-				_CPS_Master.py1 = _CPS_Master.py1 + _CPS_Master.cellSize
-			end
-		end
-	end
-	for i=1,OldPermaShowCount do
-		if IsPermaShowStatusOn[_CPS_Master.py2] == true then
-			ColorVar = _CPS_Master.color.green
-			TextVar = "      ON"
-		elseif IsPermaShowStatusOn[_CPS_Master.py2] == false then
-			ColorVar = _CPS_Master.color.lgrey
-			TextVar = "      OFF"
-		else
-			ColorVar = _CPS_Master.color.lgrey
-			TextVar = IsPermaShowStatusOn[_CPS_Master.py2]
-		end
-		if not _G.HideCustomPermaShow and string.len(OldPermaShowTable[i].Arg1) > 4 then
-			DrawCustomLine(_CPS_Master.px - 1, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.px + _CPS_Master.row - 1, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.cellSize, _CPS_Master.color.lgrey)
-			DrawCustomText(OldPermaShowTable[i].Arg1, _CPS_Master.fontSize, _CPS_Master.px, _CPS_Master.py1, _CPS_Master.color.grey)
-			DrawCustomLine(_CPS_Master.px + _CPS_Master.row, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.px + _CPS_Master.width + 1, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.cellSize, (ColorVar))
-			DrawCustomText(TextVar, _CPS_Master.fontSize, _CPS_Master.px + _CPS_Master.row + 1, _CPS_Master.py1, _CPS_Master.color.grey)
+	_CPS_Master.py1 = _G.CPS.StartY + _CPS_Master.cellSize
+	for i = 1, #_G.CPS.Index do
+		if _G.CPS.Index[i].VisibleVar and not _G.HidePermaShow[_G.CPS.Index[i].TextVar] then
+			ValueTextVar, ColorVar = _GetPermaColor(_G.CPS.Index[i])
+			DrawLine(_CPS_Master.px - 1, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.px + _CPS_Master.row - 1, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.cellSize, _CPS_Master.color.lgrey)
+			_DrawText(_G.CPS.Index[i].TextVar, _CPS_Master.fontSize, _CPS_Master.px, _CPS_Master.py1, _CPS_Master.color.grey)
+			DrawLine(_CPS_Master.px + _CPS_Master.row, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.px + _CPS_Master.width + 1, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.cellSize, ColorVar)
+			_DrawText(ValueTextVar, _CPS_Master.fontSize, _CPS_Master.px + _CPS_Master.row + 1, _CPS_Master.py1, _CPS_Master.color.grey)
 			_CPS_Master.py1 = _CPS_Master.py1 + _CPS_Master.cellSize
-			_CPS_Master.py2 = _CPS_Master.py2 + _CPS_Master.cellSize
+		end
+	end
+	for i = 1, #_G.CPS.NoIndex do
+		if _G.CPS.NoIndex[i].VisibleVar and not _G.HidePermaShow[_G.CPS.NoIndex[i].TextVar] then
+			ValueTextVar, ColorVar = _GetPermaColor(_G.CPS.NoIndex[i])
+			DrawLine(_CPS_Master.px - 1, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.px + _CPS_Master.row - 1, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.cellSize, _CPS_Master.color.lgrey)
+			_DrawText(_G.CPS.NoIndex[i].TextVar, _CPS_Master.fontSize, _CPS_Master.px, _CPS_Master.py1, _CPS_Master.color.grey)
+			DrawLine(_CPS_Master.px + _CPS_Master.row, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.px + _CPS_Master.width + 1, _CPS_Master.py1 + _CPS_Master.midSize, _CPS_Master.cellSize, ColorVar)
+			_DrawText(ValueTextVar, _CPS_Master.fontSize, _CPS_Master.px + _CPS_Master.row + 1, _CPS_Master.py1, _CPS_Master.color.grey)
+			_CPS_Master.py1 = _CPS_Master.py1 + _CPS_Master.cellSize
 		end
 	end
 end
 
-function _DrawText(Arg1, Arg2, Arg3, Arg4, Arg5)
-	_CPS_Master = GetSave("scriptConfig")["Master"]
-	_CPS_Master.row = (WINDOW_W and math.round(WINDOW_W / 6.4) or 160) * 0.7
-	if Arg3 == _CPS_Master.px then
-		if not (_G.HidePermaShow[Arg1] ~= nil and _G.HidePermaShow[Arg1] == true) then
-			if not OldPermaShowTable[Arg1] then
-				OldPermaShowTable[Arg1] = true
-				OldPermaShowCount = OldPermaShowCount + 1
-				OldPermaShowTable[OldPermaShowCount] = {}
-				OldPermaShowTable[OldPermaShowCount]["Status"] = true
-				OldPermaShowTable[OldPermaShowCount]["Arg1"] = Arg1
-				OldPermaShowTable[OldPermaShowCount]["Arg2"] = Arg2
-				OldPermaShowTable[OldPermaShowCount]["Arg3"] = Arg3
-				OldPermaShowTable[OldPermaShowCount]["Arg4"] = Arg4
-				OldPermaShowTable[OldPermaShowCount]["Arg5"] = Arg5
+function DrawText(Arg1, Arg2, Arg3, Arg4, Arg5)
+	_G.CPS.GetSaveMenu = GetSave("scriptConfig")["Menu"]
+
+	if IsKeyDown(_G.CPS.GetSaveMenu.menuKey) and IsKeyDown(1) then
+		_G.CPS.WaitForRelease = true
+	end
+
+	if not (IsKeyDown(_G.CPS.GetSaveMenu.menuKey) and IsKeyDown(1)) and _G.CPS.WaitForRelease then
+		_G.CPS.WaitForRelease = false
+		_G.CPS.OldCountDone = false
+		_G.CPS.StartY = 0
+		DelayAction(function() 	_G.CPS.OldCountDone = true end, 0.5)
+	end
+
+	if not _G.CPS.OldCountDone then
+		_CPS_Master = GetSave("scriptConfig")["Master"]
+		if Arg3 == _CPS_Master.px then
+			if Arg4 > _G.CPS.StartY then
+				_G.CPS.StartY = Arg4
 			end
 		end
-	elseif Arg3 == (_CPS_Master.px + _CPS_Master.row + 1) then
-		if Arg1 == "      ON" then
-			IsPermaShowStatusOn[Arg4] = true
-		elseif Arg1 == "      OFF" then
-			IsPermaShowStatusOn[Arg4] = false
-		else
-			IsPermaShowStatusOn[Arg4] = Arg1
-		end
-	else
-		DrawCustomText(Arg1, Arg2, Arg3, Arg4, Arg5)
 	end
-end
 
-function _DrawLine(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)
-	_CPS_Master = GetSave("scriptConfig")["Master"]
-	_CPS_Master.row = (WINDOW_W and math.round(WINDOW_W / 6.4) or 160) * 0.7
-	if not (Arg1 == (_CPS_Master.px - 1) or Arg1 == (_CPS_Master.px + _CPS_Master.row)) then
-		DrawCustomLine(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)
-	end
+	_DrawText(Arg1, Arg2, Arg3, Arg4, Arg5)
 end
